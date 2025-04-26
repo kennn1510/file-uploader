bb0578b2db439ccaf390f988e2221a273f7ff075
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-04-26 17:14:46 -0500

    createMany

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require("./generated/prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.create({
-    data: {
-      
-    }
+  await prisma.user.deleteMany();
+  const user = await prisma.user.createMany({
+    data: [
+      {
+        name: "Ken",
+        email: "ken@test.com",
+        age: 23,
+      },
+      {
+        name: "Sally",
+        email: "sally@test.com",
+        age: 32,
+      },
+    ],
   });
+
+  console.log(user);
 }
 
 main()

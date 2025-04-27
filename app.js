09701f2502e3735f4b2b9dbf0381d992f8fa8c3e
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-04-27 13:16:38 -0500

    add passport localstrategy, serialize/deserialize user, home and login ejs/routes

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const LocalStrategy = require("passport-local").LocalStrategy;
 const bcrypt = require("bcryptjs");
 const path = require("path");
 const passport = require("passport");
+const homeRouter = require("./routes/home");
+const loginRouter = require("./routes/login");
 
 const app = express();
 const prisma = new PrismaClient();
@@ -32,7 +34,63 @@ app.use(
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, "public")));
 
-passport.use(new LocalStrategy());
+passport.use(
+  new LocalStrategy(async (email, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+
+      if (!user) {
+        return done(null, false, {
+          message:
+            "There is no account associated with this email. Do you want to sign up?",
+        });
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatch) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+// passport.serializeUser((user, done) => {
+//   // might not be secure to have email here, just try being able to access the email
+//   // should be able to access through req.user
+//   return done(null, { id: user.id, email: user.email });
+// });
+
+// passport.deserializeUser(async (id, done) => {
+//   try {
+//   } catch (error) {
+//     done(error);
+//   }
+// });
+
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture,
+    });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
 
 async function main() {
   const user = await prisma.user.findMany();
@@ -48,9 +106,8 @@ main()
     await prisma.$disconnect();
   });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/", homeRouter);
+app.use("/login", loginRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}!`);

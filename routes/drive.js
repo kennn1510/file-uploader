d88936c8eb0df114e5c9a26d4c7e037f23f980fa
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-05-05 12:36:46 -0500

    Refactor file upload handling and update form action in drive view

--- routes/drive.js
+++ routes/drive.js
@@ -1,7 +1,24 @@
 const { Router } = require("express");
 const router = Router();
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const path = require("path");
+const fs = require("fs");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const uploadDir = path.join(__dirname, "../uploads");
+    fs.mkdirSync(uploadDir, { recursive: true });
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
+const upload = multer({ storage: storage });
 
 const userFolders = {};
 
@@ -24,6 +41,7 @@ router.post("/folder", (req, res) => {
 router.post("/upload", upload.single("uploaded_file"), (req, res) => {
   console.log(req.file);
   console.log(req.body);
+  res.send("Success!");
 });
 
 module.exports = router;
